e84a788a3c7d74b635c95f82ab5ab2f5c578388d
Ivan <itverdyy@gmail.com>
2024-10-06 15:30:07 +0300

    created abstract class Stone

--- src/main/java/com/lab1/stone/Stone.java
+++ src/main/java/com/lab1/stone/Stone.java
@@ -1,4 +1,38 @@
 package com.lab1.stone;
 
-public class Stone {
+public abstract class Stone {
+    private String name;
+    private double carat;
+    private double cost;
+    private double purity;
+
+    public Stone(String name, double carat, double cost, double clarity) {
+        this.name = name;
+        this.carat = carat;
+        this.cost = cost;
+        this.purity = clarity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCarat() {
+        return carat;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getPurity() {
+        return purity;
+    }
+
+    public abstract double calculateValue();
+
+    @Override
+    public String toString() {
+        return "Stone: name=" + name + ", carat=" + carat + ", cost=" + cost + ", purity=" + purity + ".";
+    }
 }
