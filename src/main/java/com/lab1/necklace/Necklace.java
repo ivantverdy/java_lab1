efcf946129f55c0c48b0a1b77596ca5996aad13b
Ivan <itverdyy@gmail.com>
2024-10-06 17:21:11 +0300

    added find stones in range function

--- src/main/java/com/lab1/necklace/Necklace.java
+++ src/main/java/com/lab1/necklace/Necklace.java
@@ -3,10 +3,12 @@ package com.lab1.necklace;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
 import com.lab1.stone.Stone;
 
 public class Necklace {
     private List<Stone> stones;
+
     public Necklace() {
         this.stones = new ArrayList<>();
     }
@@ -15,15 +17,25 @@ public class Necklace {
         stones.add(stone);
     }
 
-    public double getWeight(){
+    public double getWeight() {
         return stones.stream().mapToDouble(Stone::getCarat).sum(); // map to double takes stone object and applies getCarat() to each one
     }
 
-    public double getCost(){
+    public double getCost() {
         return stones.stream().mapToDouble(Stone::getCost).sum();
     }
 
-    public void sortByValue(){
+    public void sortByValue() {
         stones.sort(Comparator.comparingDouble(Stone::calculateValue));
+    }
+
+    public List<Stone> getStonesInPurityRange(double min, double max) {
+        List<Stone> stonesInRange = new ArrayList<>();
+        for (Stone stone : stones) {
+            if (stone.getPurity() >= min && stone.getPurity() <= max) {
+                stonesInRange.add(stone);
+            }
+        }
+        return stonesInRange;
     }
 }
